b5b4344ac5cdef36d1da3299f581500eec4b099e
Banglanfeng Pan <panbanglanfeng@gmail.com>
2019-10-27 17:53:00 -0400

    added inclass working files

--- pandas/Data_Clean/data_clean.py
+++ pandas/Data_Clean/data_clean.py
@@ -1,29 +1,15 @@
-<<<<<<< HEAD
-# %%
-import pandas as pd
-import os
-import re
-import matplotlib.pyplot as plt
-import seaborn
-
-# %%
-=======
 # %% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
 # ms-python.python added
 import os
->>>>>>> bb13863a958036ba06d659b9d9dde6a4d9e222f9
 try:
     os.chdir(os.path.join(os.getcwd(), 'pandas/Data_Clean'))
     print(os.getcwd())
 except:
     pass
 
-<<<<<<< HEAD
-=======
 # %% Import all packages
 import pandas as pd
 
->>>>>>> bb13863a958036ba06d659b9d9dde6a4d9e222f9
 # %% Import Data
 df = pd.read_csv('./data/sales.csv')
 df.head()
@@ -31,63 +17,6 @@ df.head()
 # %%
 df.shape
 
-<<<<<<< HEAD
-# %%
-df.dtypes
-
-# %% Fix column datatypes
-df['ordered_at'] = pd.to_datetime(df['ordered_at'])
-for column in ['price', 'line_total']:
-    df[column] = df[column].apply(lambda x: float(x[1:]))
-
-df.dtypes
-# %% Duplicate Rows?
-df[df.duplicated()].shape[0]
-df = df.drop_duplicates()
-
-# %% Deal with null values
-df.isnull().sum()
-# %%
-df[df['name'].isnull()].head()
-
-# %% Missing name has nothing to do with the rest data, remove
-df = df.dropna()
-
-# %% Sanity check for value ranges and to check assumptions
-
-# Some math issue?
-df[(df['price'] * df['quantity']) != df['line_total']].shape[0]
-
-# %% Only keep those valid line_total
-df = df[(df['price'] * df['quantity']) == df['line_total']]
-df.describe()
-# %% Line total less than 0??
-df[df['line_total'] < 0].shape[0]
-
-# %% Only keep those valid
-df = df[df['line_total'] >= 0]
-df.describe()
-
-# %% Text Clean up
-pattern = r'^"([A-Z ]+)" (.*)'
-
-
-def transform_func(x): return re.findall(pattern, x)[0]
-
-
-df[['category', 'name']] = df['name'].apply(transform_func).apply(pd.Series)
-
-
-# %%
-f, ax = plt.subplots(figsize=(8, 6))
-df.groupby('category')['line_total'].sum().sort_values(
-    ascending=False).plot(kind='bar')
-f.autofmt_xdate()
-plt.show()
-
-# %% Save the clean data into json
-df.to_json('./data/sales_clean.json', orient='records')
-=======
 
 # %%
 df.dtypes
@@ -138,4 +67,3 @@ df.to_json('./data/sales_clean_obj.json', orient='records')
 df.to_csv('./data/sales_clean.csv', index=None)
 
 # %%
->>>>>>> bb13863a958036ba06d659b9d9dde6a4d9e222f9
