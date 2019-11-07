d6d55185ac58ed6650f6340932c55403b92a2f08
Banglanfeng Pan <panbanglanfeng@gmail.com>
2019-11-06 22:12:24 -0500

    added a few more charts

--- web/chartjs_basics/js/basic_charts.js
+++ web/chartjs_basics/js/basic_charts.js
@@ -146,18 +146,167 @@ new Chart(document.getElementById("doughnut-chart"), {
   type: 'doughnut',
   data: {
     labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-    datasets: [
-      {
-        label: "Population (millions)",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,734,784,433]
-      }
-    ]
+    datasets: [{
+      label: "Population (millions)",
+      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+      data: [2478, 5267, 734, 784, 433]
+    }]
   },
   options: {
     title: {
       display: true,
       text: 'Predicted world population (millions) in 2050'
+    }
+  }
+});
+
+//HorizontalBar Chart
+new Chart(document.getElementById("bar-chart-horizontal"), {
+  type: 'horizontalBar',
+  data: {
+    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+    datasets: [{
+      label: "Population (millions)",
+      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+      data: [2478, 5267, 734, 784, 433]
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050'
+    }
+  }
+});
+
+//Grouped Bar Chart
+new Chart(document.getElementById("bar-chart-grouped"), {
+  type: 'bar',
+  data: {
+    labels: ["1900", "1950", "1999", "2050"],
+    datasets: [{
+      label: "Africa",
+      backgroundColor: "#3e95cd",
+      data: [133, 221, 783, 2478]
+    }, {
+      label: "Europe",
+      backgroundColor: "#8e5ea2",
+      data: [408, 547, 675, 734]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Population growth (millions)'
+    }
+  }
+});
+
+//Mixed Chart
+new Chart(document.getElementById("mixed-chart"), {
+  type: 'bar',
+  data: {
+    labels: ["1900", "1950", "1999", "2050"],
+    datasets: [{
+      label: "Europe",
+      type: "line",
+      borderColor: "#8e5ea2",
+      data: [408, 547, 675, 734],
+      fill: false
+    }, {
+      label: "Africa",
+      type: "line",
+      borderColor: "#3e95cd",
+      data: [133, 221, 783, 2478],
+      fill: false
+    }, {
+      label: "Europe",
+      type: "bar",
+      backgroundColor: "rgba(0,0,0,0.2)",
+      data: [408, 547, 675, 734],
+    }, {
+      label: "Africa",
+      type: "bar",
+      backgroundColor: "rgba(0,0,0,0.2)",
+      backgroundColorHover: "#3e95cd",
+      data: [133, 221, 783, 2478]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Population growth (millions): Europe & Africa'
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+//Bubble
+new Chart(document.getElementById("bubble-chart"), {
+  type: 'bubble',
+  data: {
+    labels: "Africa",
+    datasets: [{
+      label: ["China"],
+      backgroundColor: "rgba(255,221,50,0.2)",
+      borderColor: "rgba(255,221,50,1)",
+      data: [{
+        x: 21269017,
+        y: 5.245,
+        r: 120
+      }]
+    }, {
+      label: ["United States"],
+      backgroundColor: "rgba(60,186,159,0.2)",
+      borderColor: "rgba(60,186,159,1)",
+      data: [{
+        x: 18561934,
+        y: 7.104,
+        r: 28.32
+      }]
+    }, {
+      label: ["Germany"],
+      backgroundColor: "rgba(0,0,0,0.2)",
+      borderColor: "#000",
+      data: [{
+        x: 3979083,
+        y: 6.994,
+        r: 7.17
+      }]
+    }, {
+      label: ["Japan"],
+      backgroundColor: "rgba(193,46,12,0.2)",
+      borderColor: "rgba(193,46,12,1)",
+      data: [{
+        x: 4931877,
+        y: 5.921,
+        r: 11.18
+      }]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050'
+    },
+    scales: {
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: "Happiness"
+        }
+      }],
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: "GDP (PPP)"
+        }
+      }]
     }
   }
 });
