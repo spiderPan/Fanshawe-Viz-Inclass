07a33f4f969a17db2354cc265749c3ce2064ef91
Banglanfeng Pan <panbanglanfeng@gmail.com>
2019-11-12 22:19:49 -0500

    conver to by country

---
+++ web/drinks/data/clean.py
@@ -0,0 +1,12 @@
+# %%
+import os
+try:
+    os.chdir(os.path.join(os.getcwd(), 'web/drinks/data'))
+    print(os.getcwd())
+except:
+    pass
+# %%
+import pandas as pd
+# %%
+drinks = pd.read_csv('./raw/drinks.csv')
+drinks.to_json('./json/drinks_by_countries.json', orient='records')
