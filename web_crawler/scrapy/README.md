35787010918d23d2c635211825d22534e2684fda
Banglanfeng Pan <panbanglanfeng@gmail.com>
2020-01-18 19:05:45 -0500

    added spider

--- web_crawler/scrapy/README.md
+++ web_crawler/scrapy/README.md
@@ -1,4 +1,11 @@
 
+# Init the project
 ```
 scrapy startproject movies_cms
+```
+
+# Run Spider
+```
+cd movies_cms
+scrapy crawl movie -o items.json -t json
 ```
--- web_crawler/scrapy/movies_cms/movies_cms/spiders/movie_spider.py
+++ web_crawler/scrapy/movies_cms/movies_cms/spiders/movie_spider.py
@@ -1,8 +1,20 @@
 import scrapy
 
+from movies_cms.items import MoviesCmsItem
+
+
 class MovieSpider(scrapy.Spider):
     name = 'movie'
     allowed_domains = ["movie.thepan.cn"]
     start_urls = [
         'https://movie.thepan.cn'
     ]
+
+    def parse(self, response):
+        movies = scrapy.Selector(response).css('.movie-item')
+
+        for movie in movies:
+            movie_item = MoviesCmsItem()
+            movie_item['title'] = movie.css('h2::text').get().strip()
+            movie_item['url'] = movie.css('a').attrib['href']
+            yield movie_item
