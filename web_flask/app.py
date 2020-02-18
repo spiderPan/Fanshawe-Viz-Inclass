677be73506a5b306cae19969398f0e24bb7a9fcf
Banglanfeng Pan <panbanglanfeng@gmail.com>
2020-02-17 23:03:01 -0500

    read from mongo

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 import json
 import requests
+from urllib.parse import quote_plus
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://admin:123@mongo:27017/movies_cms"
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -13,6 +17,7 @@ def hello():
     }
     response = requests.get('http://scrapy:9080/crawl.json', params)
     data = json.loads(response.text)
-    result = '\n'.join('<p><b>{}</b> - {}</p>'.format(item['title'], item['url'])
-                       for item in data['items'])
+    movies = mongo.db.movies.find({"year": "2013"})
+    result = '\n'.join(
+        '<p><b>{}</b> - {}</p>'.format(item['title'], item['url']) for item in movies)
     return result
