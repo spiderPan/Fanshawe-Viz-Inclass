274a9e66f959e36becf51d65b7509ccaf528c357
Banglanfeng Pan <panbanglanfeng@gmail.com>
2020-02-17 20:33:04 -0500

    build scrapyrt

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
-
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from fanshawe.spiders.fanshaweSpider import FanshawespiderSpider
+import json
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
-
 def hello():
-    process = CrawlerProcess(get_project_settings())
-
-    # process.crawl(FanshawespiderSpider)
-    # process.start()  # the script will block here until the crawling is finished
-    return 'Start Spider.\n'
+    params = {
+        'spider_name': 'fanshaweSpider',
+        'start_requests': True
+    }
+    response = requests.get('http://scrapy:9080/crawl.json', params)
+    data = json.loads(response.text)
+    result = '\n'.join('<p><b>{}</b> - {}</p>'.format(item['code'], item['coordinator'])
+                       for item in data['items'])
+    return result
