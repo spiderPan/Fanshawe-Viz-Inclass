5d3046e83895ab3e48f521727491a0835211b4de
Pan <panb@tbkcreative.com>
2020-03-01 22:02:19 -0500

    added time barchart

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,16 +1,19 @@
 from flask import Flask
+from flask import render_template
 from flask_pymongo import PyMongo
 import json
 import requests
 from urllib.parse import quote_plus
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://admin:123@mongo:27017/movies_cms"
 mongo = PyMongo(app)
 
 
-@app.route('/')
-def hello():
+@app.route('/crawl')
+def crawl():
     params = {
         'spider_name': 'movie',
         'start_requests': True
@@ -21,3 +24,20 @@ def hello():
     result = '\n'.join(
         '<p><b>{}</b> - {}</p>'.format(item['title'], item['url']) for item in movies)
     return result
+
+
+@app.route('/time')
+def chart():
+    legend = 'Crawling Movie Count'
+    labels = []
+    values = []
+    for time in mongo.db.movies.distinct('time'):
+        labels.append(datetime.fromtimestamp(int(time), pytz.timezone(
+            'America/Toronto')))
+        values.append(mongo.db.movies.find({"time": time}).count())
+    return render_template('time_chart.html', values=values, labels=labels, legend=legend)
+
+
+@app.route('/')
+def hello():
+    return 'This is a Flask CMS'
