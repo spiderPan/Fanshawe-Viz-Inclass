7fe503c900569a679ef44d8329ffe4c26e374596
Pan <panb@tbkcreative.com>
2020-03-23 22:49:29 -0400

    added cleaned dashboar js

--- web_flask/static/js/dashboard.js
+++ web_flask/static/js/dashboard.js
@@ -1,6 +1,5 @@
 /* globals Chart:false, feather:false */
 
-<<<<<<< HEAD
 (function() {
     'use strict'
 
@@ -52,57 +51,3 @@
         }
     })
 }())
-=======
-(function () {
-    'use strict'
-  
-    feather.replace()
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        },
-        legend: {
-          display: false
-        }
-      }
-    })
-  }())
-  
->>>>>>> 9e748d0eb6945800d3f02a7584236d40dfbf0130
