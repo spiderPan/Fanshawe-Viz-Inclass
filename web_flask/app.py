f372963c648c901eae17847cfa914b90fa6f4a6b
Pan <panb@tbkcreative.com>
2020-03-23 22:57:30 -0400

    added yarn lock

--- web_flask/app.py
+++ web_flask/app.py
@@ -5,18 +5,23 @@ import requests
 from datetime import datetime
 import pytz
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='./static')
 app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
+<<<<<<< HEAD
     return render_template('home.html')
 
 @app.route('/bar-chart')
 def bar_chart():
     return render_template('barchart.html')
+=======
+    return render_template('index.html')
+>>>>>>> add world data
 
 
 @app.route('/fetch')
