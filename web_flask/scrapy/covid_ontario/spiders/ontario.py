bc6fd664a8d445ec4c43387449ecc5d49f253169
Pan <panb@tbkcreative.com>
2020-03-29 18:33:27 -0400

    switch xpath

--- web_flask/scrapy/covid_ontario/spiders/ontario.py
+++ web_flask/scrapy/covid_ontario/spiders/ontario.py
@@ -2,6 +2,7 @@
 import scrapy
 from covid_ontario.items import CovidOntarioStatusItem, CovidOntarioCasesItem
 from scrapy.utils.response import open_in_browser
+import datetime
 
 
 class OntarioSpider(scrapy.Spider):
@@ -14,38 +15,30 @@ class OntarioSpider(scrapy.Spider):
     def parse(self, response):
         status_item = CovidOntarioStatusItem()
         status_dict = {
-            'negative': 'Negative',
-            'pending': 'investigation',
-            'confirmed': 'Confirmed',
-            'resolved': 'Resolved',
-            'total': 'Total',
+            'Negative': 'negative',
+            'investigation': 'pending',
+            'Confirmed': 'confirmed',
+            'Resolved': 'resolved',
+            'Total': 'total',
         }
-        status_table = response.css('.field-type-text-with-summary table')[0]
+        # status_table = response.css('.field-type-text-with-summary table')[0]
         # self.logger.warning('Table HTML %s', response)
         # open_in_browser(response)
-        date_strings = [x.strip() for x in response.css(
-            '#last-modified::text').get().split(':')]
-        status_item['date'] = date_strings[1]
-        for row in status_table.css('tr'):
-            for key, name in status_dict.items():
-                row_name = row.css('td:first-child::text').get()
-                if row_name and name in row_name:
-                    value = int(
-                        row.css('td:last-child::text').get().replace(',', '')
-                    )
+        update_date = response.xpath(
+            '//abbr[@title="Eastern Time"][1]/../text()')
+        if update_date:
+            date_strings = [x.strip() for x in update_date.get().split(': ')]
+            date_string = date_strings[1].replace('.', '')
+            date_timestamp = datetime.datetime.strptime(
+                date_string, "%B %d, %Y at %I:%M %p").timestamp()
+            status_item['date'] = date_timestamp
+        for row in response.xpath('//table[1]//tr'):
+            name = row.xpath('td[1]/descendant-or-self::*/text()').get()
+            value = row.xpath('td[2]/descendant-or-self::*/text()').get()
+            for label, key in status_dict.items():
+                if name and value and label in name:
+                    value = int(value.replace(',', ''))
+                    self.logger.warning('Table Name %s', name)
+                    self.logger.warning('Label %s', label)
                     status_item[key] = value
         yield status_item
-
-        # case_table = response.css('.field-type-text-with-summary table')[1]
-        # #Debug in Browser
-        # for row in case_table.css('tbody tr'):
-        #     row_data = row.css('td::text').getall()
-        #     case_item = CovidOntarioCasesItem()
-        #     case_item['case_number'] = row_data[0]
-        #     case_item['age_and_gender'] = row_data[1]
-        #     case_item['public_health_unit'] = row_data[2]
-        #     case_item['hospital'] = row_data[3]
-        #     case_item['transmission'] = row_data[4]
-        #     case_item['status'] = row_data[5]
-        #     case_item['date'] = date_strings[1]
-        #     yield case_item
